91ef04b2fc97f73bdc293c188a594860db8d49ee
devfubar <chris@hevnly.com>
2015-05-30 14:16:23 +0100

    Seperated out the app log, event log and the listener log into seperate logs

--- eevyLog.go
+++ eevyLog.go
@@ -15,30 +15,65 @@ type EevyLog struct {
 }
 
 func NewLogger() *EevyLog {
+
 	log := EevyLog{
 		AppLog:      logging.Logger{Module: "appLog"},
 		EventLog:    logging.Logger{Module: "eventLog"},
 		ListenerLog: logging.Logger{Module: "listnerLog"},
 	}
-	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
-	backend1Leveled := logging.AddModuleLevel(backend1)
-	backend1Leveled.SetLevel(logging.ERROR, "")
-
-	log.AppLog.SetBackend(backend1Leveled)
+	log.buildAppLog()
+	log.buildEventLog()
+	log.buildListenerLog()
 
 	return &log
 }
 
+func (this *EevyLog) buildAppLog() {
+
+	appBe := logging.NewLogBackend(os.Stdout, "", 0)
+	var appFormat = logging.MustStringFormatter(
+		"%{time} %{level} %{message}",
+	)
+	appBeFormatter := logging.NewBackendFormatter(appBe, appFormat)
+	appLeveled := logging.AddModuleLevel(appBeFormatter)
+	appLeveled.SetLevel(logging.DEBUG, "")
+	this.AppLog.SetBackend(appLeveled)
+}
+
+func (this *EevyLog) buildEventLog() {
+
+	evtBe := logging.NewLogBackend(os.Stdout, "", 0)
+	var evtFormat = logging.MustStringFormatter(
+		"%{time} %{message}",
+	)
+	evtBeFormatter := logging.NewBackendFormatter(evtBe, evtFormat)
+	evtLeveled := logging.AddModuleLevel(evtBeFormatter)
+	evtLeveled.SetLevel(logging.DEBUG, "")
+	this.EventLog.SetBackend(evtLeveled)
+}
+
+func (this *EevyLog) buildListenerLog() {
+
+	listBe := logging.NewLogBackend(os.Stdout, "", 0)
+	var listFormat = logging.MustStringFormatter(
+		"%{time} %{message}",
+	)
+	listBeFormatter := logging.NewBackendFormatter(listBe, listFormat)
+	listLeveled := logging.AddModuleLevel(listBeFormatter)
+	listLeveled.SetLevel(logging.DEBUG, "")
+	this.ListenerLog.SetBackend(listLeveled)
+}
+
 func (this *EevyLog) Event(evt logger.Event) {
-	this.Critical("%s %s", evt.GetName(), evt.GetId())
+	this.EventLog.Critical("%s %s", evt.GetName(), evt.GetId())
 }
 
 func (this *EevyLog) Listener(l logger.Listener, e logger.Event) {
-	this.Critical("%s %s %s", l.GetType(), e.GetName(), e.GetId())
+	this.ListenerLog.Info("EXEC %s %s %s", l.GetType(), e.GetName(), e.GetId())
 }
 
 func (this *EevyLog) ListenerError(l logger.Listener, e logger.Event) {
-
+	this.ListenerLog.Error("ERROR %s %s %s", l.GetType(), e.GetName(), e.GetId())
 }
 
 func (this *EevyLog) Critical(format string, args ...interface{}) {
