e82edf7bac701a89cedf6d5c727f227be3c0a155
devfubar <chris@hevnly.com>
2015-06-09 11:19:00 +0100

    Added version number and ability to configure config file

--- eevy.go
+++ eevy.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"sync"
 
 	"github.com/hevnly/eevy/config"
@@ -9,14 +11,27 @@ import (
 	"github.com/hevnly/eevy/source"
 )
 
+var version string
+
 func main() {
 
-	confPath := "./conf.yml"
+	var verFlag bool
+	flag.BoolVar(&verFlag, "version", false, "Returns the version number")
+	var confPath string
+	flag.StringVar(&confPath, "conf", "conf.yml", "Location on config file (default conf.yml)")
+	flag.Parse()
+
+	// if the user wants to see the version
+	if verFlag {
+		fmt.Println(version)
+		return
+	}
 
 	c := config.Config{}
 	c.LoadFromFile(confPath)
 
 	log := NewLogger(&c.Logs)
+	log.Info("eevy starting (v%s)", version)
 	log.Info("Reading configuration file %s", confPath)
 
 	handlers := handler.BuildFromConf(c.Handlers, log)
